2c6df0b98660f9e0d3b14491aea4b3e0aba2f8bd
Dag <me@dvikan.no>
2020-11-17 02:39:06 +0100

    fix: make rss handle atom too

--- src/Rss.php
+++ src/Rss.php
@@ -1,14 +1,14 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace dvikan\SimpleParts;
 
-use DateTimeImmutable;
+use DateTime;
 use SimpleXMLElement;
 
 final class Rss
 {
+    const DATE_FORMAT = 'Y-m-d H:i:s';
+
     public static function fromUrl(string $url): array
     {
         $httpClient = HttpClient::fromOptions();
@@ -18,31 +18,78 @@ final class Rss
         return self::fromXml($response->body());
     }
 
+    public static function fromFile(string $file): array
+    {
+        $xml = file_get_contents($file);
+        if ($xml === false) {
+            throw new SimpleException();
+        }
+        return self::fromXml($xml);
+    }
+
     public static function fromXml(string $xml): array
     {
         $xml = new SimpleXMLElement($xml);
 
+        if (isset($xml->channel)) {
+            $feed = self::fromRss($xml);
+        } else {
+            $feed = self::fromAtom($xml);
+        }
+
+        usort($feed['items'], function ($a, $b) {
+            return $b['date'] <=> $a['date'];
+        });
+
+        return $feed;
+    }
+
+    private static function fromRss($xml)
+    {
         $channel = [
             'title'         => (string) $xml->channel->title,
-            'description'   => (string) $xml->channel->description,
+            'link'          => (string) $xml->channel->link,
             'items'         => [],
         ];
 
         foreach ($xml->channel->item as $item) {
-            $guid = (string) $item->guid;
-            $date = new DateTimeImmutable((string)$item->pubDate);
-
-            $item = [
-                'title'     => (string) $item->title,
-                'category'  => (string) $item->category,
-                'link'      => (string) $item->link,
-                'date'      => $date->format('Y-m-d H:i:s'),
-                'guid'      => $guid,
+            $_item = [
+                'title'         => (string) $item->title,
+                'link'          => (string) $item->link,
+                'date'          => DateTime::createFromFormat(DATE_RSS, (string) $item->pubDate)->format(self::DATE_FORMAT),
+                'description'   => (string) $item->description,
             ];
 
-            $channel['items'][$guid] = $item;
+            $channel['items'][] = $_item;
         }
 
         return $channel;
+    }
+
+    private static function fromAtom(SimpleXMLElement $xml)
+    {
+        $feed = [
+            'title'         => (string) $xml->title,
+            'link'          => (string) $xml->link,
+            'items'         => [],
+        ];
+
+        foreach ($xml->entry as $entry) {
+            $item = [
+                'title'         => (string) $entry->title,
+                'link'          => (string) $entry->link['href'],
+                'description'   => (string) $entry->content,
+            ];
+
+            if ($entry->published) {
+                $item['date'] = DateTime::createFromFormat(DATE_ATOM, (string) $entry->published)->format(self::DATE_FORMAT);
+            } else {
+                $item['date'] = DateTime::createFromFormat(DATE_ATOM, (string) $entry->updated)->format(self::DATE_FORMAT);
+            }
+
+            $feed['items'][] = $item;
+        }
+
+        return $feed;
     }
 }
