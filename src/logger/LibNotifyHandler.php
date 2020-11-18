bfc78dc23ec75bfb8eea19e60203fb3b28824bee
Dag <me@dvikan.no>
2020-11-18 14:20:35 +0100

    feat: add log level filter to libnotify handler

--- src/logger/LibNotifyHandler.php
+++ src/logger/LibNotifyHandler.php
@@ -4,8 +4,19 @@ namespace dvikan\SimpleParts;
 
 class LibNotifyHandler
 {
+    private $level;
+
+    public function __construct(int $level = Logger::INFO)
+    {
+        $this->level = $level;
+    }
+
     public function handle(array $record)
     {
+        if ($record['level'] < $this->level) {
+            return;
+        }
+
         $title = escapeshellarg($record['channel'] . '.' . $record['level_name']);
         $message = escapeshellarg($record['message']);
 
